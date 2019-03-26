321f52ec10e791b3a90f4c00b6f3b97d02ea5f6d
jevgarrido <37076070+jevgarrido@users.noreply.github.com>
2019-03-26 22:23:22 +0100

    Update dice_probability_main.m

--- dice_probability_main.m
+++ dice_probability_main.m
@@ -1,11 +1,11 @@
-function [] = dice_probability_main()
+function [] = dice_probability_main(n, f)
     % This function plots the discrete PDF associated
     % with the rolling of n dice with f faces each.
     
     % Define the Problem
     global DATA
-    DATA.n          = 3;     % Number of dice
-    DATA.f          = 6;     % Number of faces in each die
+    DATA.n          = n;     % Number of dice
+    DATA.f          = f;     % Number of faces in each die
     
     % Define the standard die
     DATA.std_die    = linspace(1, DATA.f, DATA.f);
